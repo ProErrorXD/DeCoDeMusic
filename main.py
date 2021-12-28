7a7d0954b655f9dae7157a8c72c198e2d66d05dd
aashish <aksraashish@gmail.com>
2021-12-28 10:02:08 -0800

    done for now

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 from datetime import time
 
 import requests
+from pyrogram.filters import bot
 from pytgcalls import idle
 
 from ProMusic.start import _human_time_duration, START_TIME
@@ -9,7 +10,6 @@ from callsmusic import run
 from ProMusic import __version__
 from pyrogram import Client, filters
 from pyrogram import Client as Bot, Client
-from pyrogram import Client as Bot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 #ported from aksraashish/testmultilanguage
